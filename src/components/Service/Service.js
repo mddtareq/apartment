7fdc9aeac376aefd719b8eb69cc1479e15560779
Mohammad Tareq <mdtareq320@gmail.com>
2020-11-17 18:25:04 +0600

    updated

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,51 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AddBooking from './AddBooking/AddBooking';
 import BookingList from './BookingList/BookingList';
 import Rent from './Rent/Rent';
-import SideNav from './SideNav/SideNav';
-import Logo from '../../Icon/Logo.png';
 import List from '../../Icon/list.jpg';
 import Plus from '../../Icon/plus 1.png';
 import rentI from '../../Icon/Group 33351.png';
 
 const Service = () => {
 
-    const list= () => {
-        document.getElementById('list').style.display='block';
-        document.getElementById('add').style.display='none';
-        document.getElementById('rent').style.display='none';
+    const list = () => {
+        document.getElementById('list').style.display = 'block';
+        document.getElementById('add').style.display = 'none';
+        document.getElementById('rent').style.display = 'none';
     }
-    const add= () => {
-        document.getElementById('list').style.display='none';
-        document.getElementById('add').style.display='block';
-        document.getElementById('rent').style.display='none';
+    const add = () => {
+        document.getElementById('list').style.display = 'none';
+        document.getElementById('add').style.display = 'block';
+        document.getElementById('rent').style.display = 'none';
     }
-    const rent= () => {
-        document.getElementById('list').style.display='none';
-        document.getElementById('add').style.display='none';
-        document.getElementById('rent').style.display='block';
+    const rent = () => {
+        document.getElementById('list').style.display = 'none';
+        document.getElementById('add').style.display = 'none';
+        document.getElementById('rent').style.display = 'block';
     }
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-3">
-                    <br/><br/>
-                    <button onClick={list} style={{border:'none',background:"none",outline:'none'}}><p> <img style={{ width: '17px', marginBottom: '5px' }} src={List} alt="" />  Booking list</p></button>
-                    <br/>
-                    <button onClick={add} style={{border:'none',background:"none",outline:'none'}}><p > <img style={{ width: '17px', marginBottom: '4px' }} src={Plus} alt="" />  Add Rent House</p></button>
-                    <br/>
-                    <button onClick={rent} style={{border:'none',background:"none",outline:'none'}}><p > <img style={{ width: '17px', marginBottom: '5px' }} src={rentI} alt="" /> My rent</p></button>
+                    <br /><br />
+                    <button onClick={list} style={{ border: 'none', background: "none", outline: 'none' }}><p> <img style={{ width: '17px', marginBottom: '5px' }} src={List} alt="" />  Booking list</p></button>
+                    <br />
+                    <button onClick={add} style={{ border: 'none', background: "none", outline: 'none' }}><p > <img style={{ width: '17px', marginBottom: '4px' }} src={Plus} alt="" />  Add Rent House</p></button>
+                    <br />
+                    <button onClick={rent} style={{ border: 'none', background: "none", outline: 'none' }}><p > <img style={{ width: '17px', marginBottom: '5px' }} src={rentI} alt="" /> My rent</p></button>
                 </div>
                 <div className="col-md-9">
                     <div id="list">
-                    <BookingList></BookingList>
+                        <BookingList></BookingList>
                     </div>
-                    <div id="rent">
-                    <Rent></Rent>
+                    <div style={{display: 'none'}} id="rent">
+                        <Rent></Rent>
                     </div>
-                    <div id="add">
-                    <AddBooking></AddBooking>
+                    <div style={{display: 'none'}} id="add">
+                        <AddBooking></AddBooking>
                     </div>
                 </div>
             </div>
